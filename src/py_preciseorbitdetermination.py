2a1d6f646a917c5fa7182bf80cc0f1956a401ee1
cfortunylombra <carlos.fortunylombrana@gmail.com>
2021-12-11 17:02:54 +0100

    Update py_preciseorbitdetermination.py

--- src/py_preciseorbitdetermination.py
+++ src/py_preciseorbitdetermination.py
@@ -3,3 +3,18 @@ Description: Environment Setup for the Precise Orbit Determination
 
 Author: C. Fortuny-Lombraña
 """
+
+########################################################################################################################
+################################################## IMPORT PACKAGES ##################################################
+########################################################################################################################
+
+import numpy as np
+from tudatpy.kernel import constants
+from tudatpy.kernel import numerical_simulation
+from tudatpy.kernel.astro import element_conversion
+from tudatpy.kernel.interface import spice_interface
+from tudatpy.kernel.numerical_simulation import environment_setup
+from tudatpy.kernel.numerical_simulation import propagation_setup
+from tudatpy.kernel.numerical_simulation import propagation
+
+spice_interface.load_standard_kernels()
