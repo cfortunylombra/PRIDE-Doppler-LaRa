21807af088779322c0a460e7d2fe5f3c3b175987
cfortunylombra <carlos.fortunylombrana@gmail.com>
2021-12-11 17:45:54 +0100

    Update py_preciseorbitdetermination.py

--- src/py_preciseorbitdetermination.py
+++ src/py_preciseorbitdetermination.py
@@ -5,16 +5,52 @@ Author: C. Fortuny-Lombraña
 """
 
 ########################################################################################################################
-################################################## IMPORT PACKAGES ##################################################
+################################################## IMPORT PACKAGES #####################################################
 ########################################################################################################################
 
 import numpy as np
 from tudatpy.kernel import constants
-from tudatpy.kernel import numerical_simulation
-from tudatpy.kernel.astro import element_conversion
 from tudatpy.kernel.interface import spice_interface
 from tudatpy.kernel.numerical_simulation import environment_setup
-from tudatpy.kernel.numerical_simulation import propagation_setup
-from tudatpy.kernel.numerical_simulation import propagation
 
+
+#from tudatpy.kernel import numerical_simulation
+#from tudatpy.kernel.astro import element_conversion
+#from tudatpy.kernel.numerical_simulation import propagation_setup
+#from tudatpy.kernel.numerical_simulation import propagation
+
+########################################################################################################################
+################################################## CONSTANTS AND VARIABLES #############################################
+########################################################################################################################
+
+# J2000 epoch
+J2000_in_Julian_days = 2451545.0
+
+# Initial date of the simulation
+start_date = 2459215.5 #in Julian days (J2000) = 01/01/2021 00:00:00
+
+# Duration of the simulation
+simulation_duration = 700*constants.JULIAN_DAY #seconds
+
+########################################################################################################################
+################################################## CREATE ENVIRONMENT ##################################################
+########################################################################################################################
+
+# Load spice kernels
 spice_interface.load_standard_kernels()
+
+# Initial and end time of the simulation
+
+simulation_start_epoch = (start_date-J2000_in_Julian_days)*constants.JULIAN_DAY #seconds
+simulation_end_epoch = simulation_start_epoch+simulation_duration #seconds
+
+# Define bodies in the simulation
+bodies_to_create = ["Saturn","Jupyter","Mars","Moon","Earth","Venus","Mercury","Sun"]
+
+
+global_frame_origin = "SSB" #Barycenter of Solar System
+global_frame_orientation = "ECLIPJ2000"
+body_settings = environment_setup.get_default_body_settings(
+    bodies_to_create, global_frame_origin, global_frame_orientation )
+
+bodies = environment_setup.create_system_of_bodies(body_settings)
